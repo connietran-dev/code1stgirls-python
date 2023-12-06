953e852c4e044c90b50dd7e0fae761e0d1c31955
Connie Tran <connietrandev@gmail.com>
2023-12-06 20:22:55 +0000

    First few Python files

---
+++ cat_food.py
@@ -0,0 +1,12 @@
+# Calculate how many cans of food you need for:
+# 10 cats
+# Use a variable for the # of cats and # of cans, a print() function to output the result
+# Extension: Change the calculation to work out the amount needed for 7 days
+
+number_of_cats = 10
+number_of_cans = 12
+number_of_days = 7
+
+total_cans = number_of_cats * number_of_cans * number_of_days
+
+print(f"Meowwww! {number_of_cats} cats need {total_cans} of cans over the course of {number_of_days} days!")
---
+++ hello.py
@@ -0,0 +1,29 @@
+print("Hello Connie! Happy Wednesday!!!!!")
+
+print('hello single quotes!')
+
+print(2)
+
+# hello this is a comment!
+
+# yay i almost forgot what the keyboard shortcut is for comments
+
+# Integers
+
+"""
+9, -24, 1040
+"""
+
+# Floats
+
+"""
+6.3, 92.0, -35.1923
+"""
+
+oranges = 12
+
+cost_per_orange = 0.5
+
+total_costs = oranges * cost_per_orange
+
+print(f'{oranges} oranges costs ${total_costs}')
