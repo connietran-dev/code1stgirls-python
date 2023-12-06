4780762faebd13e0004b60990386e7b41a5733cf
Connie Tran <connietrandev@gmail.com>
2023-12-06 20:42:02 +0000

    Added alternative to f-string

--- cat_food.py
+++ cat_food.py
@@ -10,3 +10,9 @@ number_of_days = 7
 total_cans = number_of_cats * cans_per_cat * number_of_days
 
 print(f"Meowwww! {number_of_cats} cats need {total_cans} of cans over the course of {number_of_days} days!")
+
+# The previous alternative to fstrings was:
+print('This is the alternative to fstrings back in the day: {} cats need {} cans for {} days'.format(number_of_cats, cans_per_cat, number_of_days))
+
+# FYI for the homework:
+# print(f'You need to buy {message} nails')
