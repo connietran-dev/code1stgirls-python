2986ad0d70c4baafb29135b322f77a079ede0d9d
Connie Tran <connietrandev@gmail.com>
2023-12-13 20:11:59 +0000

    Session 2 on while loops

---
+++ session2-while.py
@@ -0,0 +1,8 @@
+club_capacity = 5
+
+while club_capacity >0:
+    print(f'Come in! We\'ve got {club_capacity}')
+    club_capacity -= 1
+    print(f'Someone left. We\'ve got {club_capacity}')
+
+print('Sorry the club is full! Please wait')
